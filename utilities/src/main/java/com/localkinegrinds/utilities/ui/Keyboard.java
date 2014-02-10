3e581f7e3c382d828d98da0654b318608d58a0e8
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-02-09 22:24:33 -0500

    Added ActivityUtilities

---
+++ utilities/src/main/java/com/localkinegrinds/utilities/ui/Keyboard.java
@@ -0,0 +1,29 @@
+package com.localkinegrinds.utilities.ui;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public final class Keyboard {
+
+    private static final String TAG = Keyboard.class.getName();
+
+    public final static boolean hideSoftInput(final View view) {
+
+        if (null == view)
+            return false;
+
+        final Context context = view.getContext();
+
+        if (null != context) {
+            Log.d(TAG, "Attempting to hide soft keyboard input");
+            InputMethodManager manager = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (null != manager) {
+                return manager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
+
+        return false;
+    }
+}
